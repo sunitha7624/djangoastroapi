fee09b16f62de97558576ae6e2208e43672420fe
Suntha N <sunitha@lenderindia.com>
2023-07-24 19:14:23 +0530

    api token

--- Core_Api/urls.py
+++ Core_Api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . views import *
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
 # from . import views as aboutview
 from django.conf.urls import include
 
@@ -18,6 +19,13 @@ router.register('dstcorrectioncanada', DstCanadaViewSet)
 
 
 urlpatterns = [
+    
+    path('api/token/',
+         jwt_views.TokenObtainPairView.as_view(),
+         name ='token_obtain_pair'),
+    path('api/token/refresh/',
+         jwt_views.TokenRefreshView.as_view(),
+         name ='token_refresh'),
     path('', include(router.urls)),
     
     
